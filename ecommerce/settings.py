2a2ed2db528e3b7c7666ae33e2a4e5d5955b917d
MarekD97 <mdorosz2@wp.pl>
2020-07-15 23:58:02 +0200

    Fixes in media_root

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -144,7 +144,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/images/')
 
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
